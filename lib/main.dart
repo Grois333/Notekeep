6043c9d6fefeeb48275e3c00eae4b4505ce6aa8b
Grois333 <isaacgroisman@gmail.com>
2023-10-03 21:32:55 +0300

    cleaning routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
-import 'dart:ffi';
-
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:notekeep/constants/routes.dart';
 import 'package:notekeep/firebase_options.dart';
 import 'package:notekeep/views/login_view.dart';
 import 'package:notekeep/views/register_view.dart';
@@ -19,9 +18,9 @@ void main() {
       ),
       home: const HomePage(),
       routes: {
-        '/login/': (context) => const LoginView(),
-        '/register/': (context) => const RegisterView(),
-        '/notes/': (context) => const NotesView(),
+        loginRoute: (context) => const LoginView(),
+        registerRoute: (context) => const RegisterView(),
+        notesRoute: (context) => const NotesView(),
       },
     ),);
 }
@@ -94,7 +93,7 @@ class _NotesViewState extends State<NotesView> {
                   final shouldLogout = await showLogOutDialog(context);
                   if(shouldLogout){
                     await FirebaseAuth.instance.signOut();
-                    Navigator.of(context).pushNamedAndRemoveUntil('/login/', (_) => false,);
+                    Navigator.of(context).pushNamedAndRemoveUntil(loginRoute, (_) => false,);
                   }
                   // devtools.log(shouldLogout.toString());
                   // break;
--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -2,6 +2,8 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'dart:developer' as devtools show log;
 
+import '../constants/routes.dart';
+
 class LoginView extends StatefulWidget {
   const LoginView({ Key? key }) : super(key: key);
 
@@ -58,7 +60,7 @@ class _LoginViewState extends State<LoginView> {
               try{
                 final userCredential = await FirebaseAuth.instance.signInWithEmailAndPassword(email: email, password: password);
                 devtools.log(userCredential.toString());
-                Navigator.of(context).pushNamedAndRemoveUntil('/notes/', (route) => false,);
+                Navigator.of(context).pushNamedAndRemoveUntil(notesRoute, (route) => false,);
                 //print(userCredential);
               } on FirebaseAuthException catch (e){
                 if(e.code == 'user-not-found'){
@@ -80,7 +82,7 @@ class _LoginViewState extends State<LoginView> {
           ),
           TextButton(
             onPressed: () {
-              Navigator.of(context).pushNamedAndRemoveUntil('/register/', (route) => false,);
+              Navigator.of(context).pushNamedAndRemoveUntil(registerRoute, (route) => false,);
             }, child: const Text('Not registered yet? Register here!')
           )
         ],
