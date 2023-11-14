e87039113b55a97d6b96c3579f0d88eadd2470da
Grois333 <isaacgroisman@gmail.com>
2023-11-14 21:23:16 +0200

    Writing Notes to Cloud Firestore

--- test/auth_test.dart
+++ test/auth_test.dart
@@ -116,7 +116,7 @@ class MockAuthProvider implements AuthProvider {
     if (email == 'foo@bar.com') throw UserNotFoundAuthException();
     if (password == 'foobar') throw WrongPasswordAuthException();
     const user = AuthUser(
-      //id: 'my_id',
+      id: 'my_id',
       isEmailVerified: false,
       email: 'foo@bar.com',
     );
@@ -138,7 +138,7 @@ class MockAuthProvider implements AuthProvider {
     final user = _user;
     if (user == null) throw UserNotFoundAuthException();
     const newUser = AuthUser(
-      //id: 'my_id',
+      id: 'my_id',
       isEmailVerified: true,
       email: 'foo@bar.com',
     );
