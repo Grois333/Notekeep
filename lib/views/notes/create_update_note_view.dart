12c6e1615b938111c4f30a48bf380176fa09cf74
Grois333 <isaacgroisman@gmail.com>
2023-11-15 21:07:59 +0200

    Sharing Notes

--- lib/views/notes/create_update_note_view.dart
+++ lib/views/notes/create_update_note_view.dart
@@ -4,6 +4,9 @@ import 'package:notekeep/services/auth/auth_service.dart';
 import 'package:notekeep/utilities/generics/get_arguments.dart';
 import 'package:notekeep/services/cloud/cloud_note.dart';
 import 'package:notekeep/services/cloud/firebase_cloud_storage.dart';
+import 'package:share_plus/share_plus.dart';
+
+import '../../utilities/dialogs/cannot_share_empty_note_dialog.dart';
 
 class CreateUpdateNoteView extends StatefulWidget {
   const CreateUpdateNoteView({ Key? key }) : super(key: key);
@@ -99,7 +102,20 @@ class _CreateUpdateNoteViewState extends State<CreateUpdateNoteView> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('New Note',)
+        title: const Text('New Note',),
+        actions: [
+          IconButton(
+            onPressed: () async {
+              final text = _textController.text;
+              if (_note == null || text.isEmpty) {
+                await showCannotShareEmptyNoteDialog(context);
+              } else {
+                Share.share(text);
+              }
+            },
+            icon: const Icon(Icons.share),
+          ),
+        ],
       ),
       body: FutureBuilder(
         future: createOrGetExistingNote(context),
